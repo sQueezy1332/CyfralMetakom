cc95fc7288df7c88adf76c303ac7bc8732cb5c18
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-02-28 15:41:17 +0300

    Update CyfralMetakomHeader.h

--- CyfralMetakomHeader.h
+++ CyfralMetakomHeader.h
@@ -1,7 +1,15 @@
 #pragma once
 #pragma GCC diagnostic ignored "-Woverflow"
 #pragma GCC diagnostic ignored "-Wparentheses"
-#define DEBUG_ENABLE
+	#define DEBUG_ENABLE
+
+#ifdef DEBUG_ENABLE
+#define DEBUG(x, ...) Serial.print(x, ##__VA_ARGS__)
+#define DEBUGLN(x, ...) Serial.println(x, ##__VA_ARGS__)
+#else
+#define DEBUG(x, ...)
+#define DEBUGLN(x, ...)
+#endif
 #include <Arduino.h>
 #include <keysniffer.h>
 #include "Sim800.h"
