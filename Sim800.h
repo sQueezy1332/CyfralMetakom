2cd080675a9759ce75817f3d15fe95be25887576
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-04-10 14:42:57 +0300

    Update Sim800.h

--- Sim800.h
+++ Sim800.h
@@ -1,18 +1,13 @@
 #pragma once
-#pragma GCC diagnostic ignored "-Wconversion-null"
-#include <Arduino.h>
+//#pragma GCC diagnostic ignored "-Wconversion-null"
+//#include <Arduino.h>
 #include <Softwareserial.h>
-#include <string.h> 
-#include <stdlib.h>
+typedef uint8_t byte;
+//#include <string.h> 
+//#include <stdlib.h>
 
-//#define DEBUG_ENABLE
-#ifdef DEBUG_ENABLE
-#define DEBUG(x) Serial.println(x)
-#else
-#define DEBUG(x)
-#endif
 #define keylen 8 
-#define buflen 40
+#define RESP_BUFLEN 64
 #define RX 10 //green TX on sim800 push-pull
 #define TX 11  //orange RX on sim800 pull-up
 #define PIN_SIM_RST 9
@@ -32,70 +27,75 @@
 #define SUB			"\x1A"	// 26
 #define ESC			"\x1B"	// 27
 
-SoftwareSerial //<1, 0> 
-sim (RX, TX);
+#define BAUDRATE 9600 //not recommend to use more 19200 if open drain uart is enabled
 
-extern volatile byte flagInterrupt;
-extern byte flagSmsNotsended;
-char respBuf[buflen] = "";
+SoftwareSerial //<1, 0> 
+sim(RX, TX);
+
+__attribute__((weak))  extern volatile byte flagInterrupt;
+__attribute__((weak))  extern byte flagSmsNotsended;
+char respBuf[RESP_BUFLEN] {};
 void initSIM();
 void resetSIM();
 
-char* waitResponse(byte timeout_sec = 5) {
-	uint32_t timeout = millis() + timeout_sec * 1000ul;
-	while (millis() < timeout) {
+bool waitResponse(size_t timeout = 5000) {
+	uint32_t timer = millis();
+	do {
 		if (sim.available()) {
-			byte amount = sim.readBytes(respBuf, buflen);
-			respBuf[amount] = NULL;
+			byte amount = sim.readBytesUntil('\n', respBuf, RESP_BUFLEN);
+			respBuf[amount] = '\0';
 			DEBUG(respBuf);
-			return respBuf;
+			return true;
 		}
-	}
+	} while (millis() - timer < timeout);
 	DEBUG(F("\tTimeout..."));
-	return nullptr;
+	return false;
 }
 
 
-bool sendAT(const char* cmd, const char* response, byte retry = 3, byte timeout_sec = 3) { //retry 0 dont do waiting
-	do sim.println(cmd);
-	while (retry && strstr(waitResponse(timeout_sec), response) == NULL && --retry);
-	return retry;
+bool sendAT(const char* cmd, const char* response, size_t timeout = 3000) { //retry 0 dont waiting
+	sim.println(cmd);
+	if (!waitResponse(timeout)) return false;
+	return !strstr(respBuf, response);
 }
 
-bool sendAT(const char* cmd, char response = OK, byte retry = 3, byte timeout_sec = 3) {  
-	do sim.println(cmd);
-	while (retry && strchr(waitResponse(timeout_sec), response) == NULL && --retry);
-	return retry;
+bool sendAT(const char* cmd, char response = OK, size_t timeout = 3000) {
+	char resp[] { response, '\0' };
+	return sendAT(cmd, resp, timeout);
 }
 
-bool SendSms(const byte pBase[][keylen], const char* send_sms, word* vArray, const byte& keysAmount, const byte& volKeysAmount) {
+bool SendSms(const byte pBase[][keylen], const char* number, word* vArray, byte keysAmount, byte volKeysAmount) {
 	if (!sendAT("AT+CCALR?", CONNECT)) {
 		resetSIM();
 		delay(5000);
 		if (!sendAT("AT+CCALR?", CONNECT)) return false;
 	}
-	char rssi[11] {};
 	sendAT("AT+CSQ", ':');
-	strlcpy(rssi, respBuf, 11);
-	if (!sendAT(send_sms, '>')) { 
+	char rssi[12];
+	strlcpy(rssi, respBuf, sizeof(rssi));
+	sim.print("AT+CMGS=\""); sim.print(number);
+	if (!sendAT("\"", '>')) {
 		resetSIM();
 		return false;
 	}
 	for (byte i = 0, j = 0; i < keysAmount; i++) {
-		while (j < 4) { sim.print(pBase[i][j++], HEX); sim.print(SPACE); }
-		sim.print('@');
-		while (j < 7) { sim.print(pBase[i][j++], DEC); sim.print(SPACE); }
+		while (j < 4) { sim.print(pBase[i][j++], HEX); sim.print(' '); }
+		sim.print('|');
+		while (j < 7) { sim.print(pBase[i][j++], DEC); sim.print(' '); }
 		sim.println(pBase[i][j], DEC); j = 0;
 	}
 	if (volKeysAmount && vArray != nullptr) {
-		sim.print("\nvolKeysAmount = "); sim.println(volKeysAmount); 
-		for (byte i = 0; i < volKeysAmount; i++) sim.println(vArray[i]); 
+		sim.print("\nvolKeysAmount = "); sim.println(volKeysAmount);
+		for (byte i = 0; i < volKeysAmount; i++) sim.println(vArray[i]);
 	}
 	if (flagSmsNotsended > 1) { sim.print("flagSmsNotsended = ");  sim.println(flagSmsNotsended, HEX); }
 	if (flagInterrupt) { sim.print("flagInterrupt = ");  sim.println(flagInterrupt); }
 	sim.print(rssi);
-	if (sendAT(SUB, "CMGS", 3, 5)) return true; 
-	else if (sendAT(ESC, '>', 1) && !strchr(waitResponse(), ERROR)) resetSIM();
+	if (sendAT(SUB, "CMGS")) return true;
+	else if (sendAT(ESC, '>')) { 
+		if (!waitResponse() || !strchr(respBuf, ERROR)) 
+			resetSIM(); 
+	}
 	return false;
 }
 /*
@@ -107,7 +107,7 @@ void available() {
 }
 */
 void initSIM() {
-	sim.begin(9600);
+	sim.begin(BAUDRATE);
 	sim.setTimeout(100);
 	delay(5000);
 	if (!sendAT("AT")) sendAT(ESC);
@@ -115,8 +115,8 @@ void initSIM() {
 	sendAT("ATE0V0+CMEE=1;+CLIP=1;+DDET=1,250;+CMGF=1;&W"); //ATE0V1
 }
 void resetSIM() {
-	pMode(PIN_SIM_RST, OUTPUT);
+	pinMode(PIN_SIM_RST, OUTPUT);
 	delay(100);
-	pMode(PIN_SIM_RST, INPUT);
+	pinMode(PIN_SIM_RST, INPUT);
 }
 
